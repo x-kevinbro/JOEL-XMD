827c1ecffe09c9eb92b913baa54ebf7e45438fe5
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2025-05-09 14:49:35 +0300

    ━ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴏʀᴅ ᴊᴏᴇʟ ━

--- config.cjs
+++ config.cjs
@@ -21,7 +21,8 @@ const config = {
   AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚', // For liking status updates(stories)
   AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS !== undefined ? process.env.AUTO_REPLY_STATUS === 'true' : false,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || 'Status Viewed by joel-Md',
-  VOICE_CHAT_BOT: process.env.VOICE_CHAT_BOT !== undefined ? process.env.CHAT === 'true' : false,
+  VOICE_CHAT_BOT: process.env.VOICE_CHAT_BOT !== undefined ? process.env.VOICE_CHAT_BOT === 'true' : false,
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true,
   ANTILINK: process.env.ANTILINK !== undefined ? process.env.ANTILINK === 'true' : false,
   ANTI_LEFT: process.env.ANTI_LEFT !== undefined ? process.env.ANTI_LEFT === 'true' : true,
   AUTO_STICKER: process.env.AUTO_STICKER !== undefined ? process.env.AUTO_STICKER === 'true' : false,
